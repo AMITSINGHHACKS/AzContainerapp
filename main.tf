158838358a5f7d490dbf7fc197b0b9b8860cc0df
amit <amitkumar.singh@vsit.edu.in>
2024-11-19 13:27:59 +0000

    added basic template

--- main.tf
+++ main.tf
@@ -1,1 +1,48 @@
+#Provider Code
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
 
+#Creating RG
+resource "azurerm_resource_group" "my_rg" {
+  name     = var.rg_name
+  location = var.rg_location
+}
+
+#AZ Container App Log Analytics
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "acctest-01"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+#Az App Env
+resource "azurerm_container_app_environment" "example" {
+  name                       = "Example-Environment"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+}
+
+#Az Container App
+resource "azurerm_container_app" "example" {
+  name                         = "example-app"
+  container_app_environment_id = azurerm_container_app_environment.example.id
+  resource_group_name          = azurerm_resource_group.example.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "examplecontainerapp"
+      image  = "mcr.microsoft.com/k8se/quickstart:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "subscription_id" {
+  type    = string
+  default = ""
+}
+variable "client_id" {
+  type    = string
+  default = ""
+}
+variable "client_secret" {
+  type    = string
+  default = ""
+}
+variable "tenant_id" {
+  type    = string
+  default = ""
+}
+variable "rg_name" {
+  type    = string
+  default = ""
+}
